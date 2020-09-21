a3b673abbb0090d87857e057045b28bb2e2d6fb1
ariaschecter <71573047+ariaschecter@users.noreply.github.com>
2020-09-21 07:48:36 +0700

    Add files via upload

---
+++ welcome.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int nama, asal;
+    cout << "Masukkan nama anda: ";
+    cin >> nama;
+    cout << "Masukkan asal anda: ";
+    cin >> asal;
+
+    cout << endl;
+    cout << "Nama anda " << nama << ", anda berasal dari " << asal << endl;
+
+
+    cin.get();
+    return 0;
+}
